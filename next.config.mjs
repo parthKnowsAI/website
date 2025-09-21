d66ebf25c453135b30172a566af3f14cb166752b
Parth Patel <patelp94@montclair.edu>
2025-09-20 21:17:28 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/website" : "",
-  assetPrefix: isProd ? "/website/" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
